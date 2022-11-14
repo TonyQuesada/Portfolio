181c645fc962bc702cd434f5be9244701b02dad5
TonyQuesada <Tonyquesadaxd@gmail.com>
2022-11-14 17:46:18 -0600

    Cambios de http a https

--- dist/assets/js/modelo_3d.js
+++ dist/assets/js/modelo_3d.js
@@ -35,7 +35,7 @@ const sizes = {
     width: width,
     height: height
     // width: window.innerWidth,
-    // height: window.innerHeight
+    // height: window.innerHeight 
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
