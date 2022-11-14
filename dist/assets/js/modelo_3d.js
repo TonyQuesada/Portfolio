7f1260bef713c9d6246f3d1e75df93d51e8e2379
TonyQuesada <Tonyquesadaxd@gmail.com>
2022-11-14 15:18:15 -0600

    Prueba para cargar el objeto 3D

---
+++ dist/assets/js/modelo_3d.js
@@ -0,0 +1,66 @@
+// import * as THREE from '../../three.js-master/build/three.module.js'
+import * as THREE from 'http://unpkg.com/three@0.127.0/build/three.module.js'
+import { GLTFLoader } from 'http://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'http://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+var root;
+
+const loader = new GLTFLoader()
+loader.load('../../../dist/assets/3D/perfil.glb', function (glb) {
+    console.log(glb)
+    root = glb.scene;
+    root.scale.set(20, 20, 20)
+    root.position.set(0, -0.15, 0)
+
+    scene.add(root);
+}, function (xhr) {
+    console.log((xhr.loader / xhr.total * 100) + "% loaded")
+}, function (error) {
+    console.log('An error occurred')
+})
+
+
+const light = new THREE.AmbientLight(0xffffff, 1)
+scene.add(light)
+
+//Boiler Plate Code
+// const canvas = renderer.domElement;
+const width = canvas.clientWidth;
+const height = canvas.clientHeight;
+
+
+const sizes = {
+    width: width,
+    height: height
+    // width: window.innerWidth,
+    // height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 0.04, 3)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas,
+    alpha: true
+})
+
+renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.gammaOuput = true
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    root.rotation.y += 0.015
+    renderer.render(scene, camera)
+    controls.update()
+    controls.enableDamping = true
+}
+
+animate()
