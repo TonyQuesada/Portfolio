f883077684053f323d4db510ed857f77fb210cef
Tony Quesada <TonyQuesadaxd@gmail.com>
2023-01-10 00:41:53 -0600

    Fix de errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,9 +196,7 @@ var btnplay = document.getElementById("play");
 var btnpause = document.getElementById("pause");
 var sound = document.getElementById("player");
 
-sound.muted = true;
 sound.play();
-sound.muted = false;
 sound.loop = true;
 sound.volume = 0.015;
 
